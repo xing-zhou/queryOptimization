8cb418fa7fe27adb0923c315aad2335ed004834d
yunfeiwang <yw2577@columbia.edu>
2015-04-18 09:47:35 -0400

    Created queryNode.java and read queries into queryList by line

--- QueryOptimization/src/helloworld.java
+++ QueryOptimization/src/helloworld.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class helloworld {
@@ -12,10 +14,10 @@ public class helloworld {
 		
 		BufferedReader br = null;
 		try {
-			String sCurrentLine;
+			String configLine;
 			br = new BufferedReader(new FileReader("./config.txt"));
-			while ((sCurrentLine = br.readLine()) != null) {
-				System.out.println(sCurrentLine);
+			while ((configLine = br.readLine()) != null) {
+				System.out.println(configLine);
 			}
 		}
 		catch (IOException e) {
@@ -29,6 +31,38 @@ public class helloworld {
 			catch (IOException ex){
 				ex.printStackTrace();
 			}
+		}
+		
+		List <queryNode> queryList = new ArrayList<queryNode>();
+		br = null;
+		try {
+			String queryLine;
+			br = new BufferedReader(new FileReader("./query.txt"));
+			while ((queryLine = br.readLine()) != null){
+				String [] parsing = queryLine.split("\\s+");
+				int i = 0;
+				queryNode node = new queryNode();
+				for (i = 0; i < parsing.length;i++){
+					node.conditions.add(Double.valueOf(parsing[i]));
+				}
+				queryList.add(node);
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (br != null)
+					br.close();
+			}
+			catch (IOException ex){
+				ex.printStackTrace();
+			}
+		}
+		for (queryNode node : queryList){
+			System.out.println(node.conditions);
+			System.out.println("hahaha");
 		}
 	}
 
---
+++ QueryOptimization/src/queryNode.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class queryNode {
+	List <Double> conditions;
+	public queryNode (){
+		conditions = new ArrayList<Double>();
+	}
+}
