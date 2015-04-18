910e7b8ebf1d773b23e23037e93428dbd679fb38
yunfeiwang <yw2577@columbia.edu>
2015-04-17 22:48:49 -0400

    added statements for read config.txt

--- QueryOptimization/src/helloworld.java
+++ QueryOptimization/src/helloworld.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class helloworld {
 
@@ -5,6 +9,27 @@ public class helloworld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Hello Again World!");
+		
+		BufferedReader br = null;
+		try {
+			String sCurrentLine;
+			br = new BufferedReader(new FileReader("./config.txt"));
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (br != null)
+					br.close();
+			}
+			catch (IOException ex){
+				ex.printStackTrace();
+			}
+		}
 	}
 
 }
