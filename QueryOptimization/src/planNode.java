c52c5c5bf38dadf208d283dbd253120fc0d1d915
yunfeiwang <yw2577@columbia.edu>
2015-04-22 21:31:24 -0400

    added comment

--- QueryOptimization/src/planNode.java
+++ QueryOptimization/src/planNode.java
@@ -10,37 +10,31 @@ public class planNode {
 	readConfigs config;//configuration file components
 	Double fcost;//fcost(E)
 	Double cost_ratio;//(p-1)/fcost(E)
-	Double combined_selectivity;
-	planNode L;
-	planNode R;
+	Double combined_selectivity;//combined selectivity of the plan
+	planNode L;//left child
+	planNode R;//right child
 	public planNode(List<basicTerm> subset, readConfigs con){
 		k = (double)subset.size();
 		config = con;
 		combined_selectivity = 1.0;
 		for (basicTerm term : subset){
 			combined_selectivity *= term.selectivity;
-		}
-		fcost = compute_fcost();
-		cost_ratio = compute_cost_ratio();
-		b = false;
-		c = computeCost(subset);
-		Double no_branch = computeNoBranchCost();
+		}//calculate combined_selectivity
+		fcost = compute_fcost();//compute the fcost
+		cost_ratio = compute_cost_ratio();//compute (p-1)/fcost(E)
+		b = false;//initialized to false
+		c = computeCost(subset);//total cost of this plan
+		Double no_branch = computeNoBranchCost();//compute the no-branch cost of this plan
 		if (no_branch < c){
 			c = no_branch;
 			b = true;
-		}
-		/*
-		System.out.print("[");
-		for (basicTerm t : subset)
-			System.out.print(t.selectivity + ", ");
-		System.out.println("]\nfinal Cost: " + c);
-		System.out.println("k = " + k);
-		System.out.println("b = " + b);
-		*/
+		}//update cost if no-branch has lower cost, and change the boolean flag
+		
 		set = subset;
-		L = null;
+		L = null;//initialize L and R to be null
 		R = null;
 	}
+	//compute the total cost of plan in branch
 	public Double computeCost(List<basicTerm> s){
 		Double q = 1.0;
 		for (basicTerm term : s){
@@ -51,22 +45,22 @@ public class planNode {
 			q = 1.0-q;
 		Double totalCost = k*config.r + (k-1)*config.l + config.f*k + config.t + config.m * q + p_product*config.a; //Cost Function from Example 5
 		totalCost = Math.round(totalCost * 1000.000)/1000.000;
-		//System.out.println("totalCost: " + totalCost + " with q = " + q);
 		return totalCost;
 	}
-	
+	//compute the total cost of plan in no-branch
 	public Double computeNoBranchCost(){
-		Double no_branch_cost = k*config.r + (k-1)*config.l + config.f*k + config.a;
+		Double no_branch_cost = k*config.r + (k-1)*config.l + config.f*k + config.a;//cost function from no-branch cost 
 		no_branch_cost = Math.round(no_branch_cost * 1000.000)/1000.000;
-		//System.out.println("no_branch Cost: " + no_branch_cost);
+		
 		return no_branch_cost;
 	}
 	
+	//compute fcost of the plan (&-term)
 	public Double compute_fcost(){
 		Double _fcost = config.r * k + (k-1) * config.l + config.f*k + config.t;
 		return _fcost;
 	}
-	
+	//compute (p-1)/fcost(E)
 	public Double compute_cost_ratio(){
 		Double cost_ratio = (combined_selectivity-1)/fcost;
 		return cost_ratio;
--- QueryOptimization/src/queryNode.java
+++ QueryOptimization/src/queryNode.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 
 public class queryNode {
+	//each queryNode contains a list of basic terms for each query
 	List <basicTerm> conditions;
 	public queryNode (){
 		conditions = new ArrayList<basicTerm>();
